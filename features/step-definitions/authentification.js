276a179913258b52bc1e4ee85dc1fe0f6315aca1
Karlis <karlisbirznieks37@gmail.com>
2024-02-20 19:25:21 +0200

    changes

--- features/step-definitions/authentification.js
+++ features/step-definitions/authentification.js
@@ -1,6 +1,6 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
-import homePage from '../page-objects/home.page.js';
-import signInPage from '../page-objects/signin.page.js';
+import homePage from '../page-objects/home-page.js';
+import signInPage from '../page-objects/signin-page.js';
 
 When('I click on the Sign In link', async function() {
     await homePage.signInLink.click();
