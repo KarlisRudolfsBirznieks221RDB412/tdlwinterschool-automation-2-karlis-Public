de398fa749c2907049815917688f7b508fe9aaab
Karlis <karlisbirznieks37@gmail.com>
2024-02-21 17:39:43 +0200

    chanes

---
+++ features/order.feature
@@ -0,0 +1,6 @@
+Feature: Orders
+
+    @wip
+    Scenario: User can make an order
+        Given I am on the home page
+        And I have logged in as Peter
---
+++ features/test-data/users.js
@@ -0,0 +1,11 @@
+
+
+export const users = {
+    peter: {
+        email: "peter.parker123456@gmail.com",
+        password: "peter.parker123456",
+        firstname: "Peter",
+        lastname: "Parker"
+}
+
+}
