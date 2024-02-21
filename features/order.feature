6370c15877b09c4028e5b300fe27191070f8504f
Karlis <karlisbirznieks37@gmail.com>
2024-02-21 18:04:50 +0200

    changes

--- features/order.feature
+++ features/order.feature
@@ -4,3 +4,4 @@ Feature: Orders
     Scenario: User can make an order
         Given I am on the home page
         And I have logged in as Peter
+        When I select "Women -> Tops -> Jackets" menu items
--- features/step-definitions/home.js
+++ features/step-definitions/home.js
@@ -1,5 +1,6 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
 import homePage from '../page-objects/home.page.js';
+import {parseMenuExpression} from 'utils.js'
 
 Given('I am on the home page', async function() {
     await homePage.open();
@@ -7,4 +8,8 @@ Given('I am on the home page', async function() {
 
 Then('I see the welcome message {string}', async function(message) {
     await expect(homePage.greetMessage).toHaveText(message);
+});
+
+When('I select {string} menu items', async function(menuExpression) {
+    const menuItems = parseMenuExpression(menuExpression); 
 });
