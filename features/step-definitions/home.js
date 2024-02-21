8984e579da78ba8c82c300a1eceed06f7f5d6032
Karlis <karlisbirznieks37@gmail.com>
2024-02-21 17:41:20 +0200

    chane

--- features/step-definitions/home.js
+++ features/step-definitions/home.js
@@ -1,5 +1,5 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
-import homePage from '../page-objects/home-page.js';
+import homePage from '../page-objects/home.page.js';
 
 Given('I am on the home page', async function() {
     await homePage.open();
