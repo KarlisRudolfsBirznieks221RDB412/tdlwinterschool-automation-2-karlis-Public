d10c652ae10a3b52f0f6d79465be515c01b47dec
Karlis <karlisbirznieks37@gmail.com>
2024-02-23 17:29:48 +0200

    final addition

--- features/step-definitions/home.js
+++ features/step-definitions/home.js
@@ -1,6 +1,7 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
+import { parseMenuExpression } from '../utils/utils.js';
 import homePage from '../page-objects/home.page.js';
-import {parseMenuExpression} from 'utils.js'
+import menuPage from '../page-objects/menu.page.js';
 
 Given('I am on the home page', async function() {
     await homePage.open();
@@ -10,6 +11,7 @@ Then('I see the welcome message {string}', async function(message) {
     await expect(homePage.greetMessage).toHaveText(message);
 });
 
-When('I select {string} menu items', async function(menuExpression) {
-    const menuItems = parseMenuExpression(menuExpression); 
+When('I select {string} menu item', async function(menuExpression) {
+    const menuItems = parseMenuExpression(menuExpression);
+    await menuPage.selectMenuItem(menuItems);
 });
